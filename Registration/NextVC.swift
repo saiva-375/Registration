14ef8ddf9f0a74dd0ded968735df731f5677823c
saiva-375 <saivadlamudi375@gmail.com>
2026-01-20 13:41:44 -0500

    Updated the navigation control with code, rather than storyboard.

---
+++ Registration/NextVC.swift
@@ -0,0 +1,29 @@
+//
+//  NextVC.swift
+//  Registration
+//
+//  Created by seshi on 1/20/26.
+//
+
+import UIKit
+
+class NextVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Registration/ViewController.swift
+++ Registration/ViewController.swift
@@ -2,18 +2,53 @@
 //  ViewController.swift
 //  Registration
 //
-//  Created by seshi on 1/16/26.
+//  Created by sai on 1/16/26.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
+    @IBOutlet weak var userNameTF: UITextField!
+    
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextButton(_ sender: Any) {
+        if !validateLogin() {
+            showAlert(title: "Login Failed", message: "Please enter valid credentials")
+            return
+        }
+        navgiateToNextScreen()
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func validateLogin() -> Bool {
+        guard let username = userNameTF.text, let password = passwordTF.text else {
+            return false
+        }
+        
+        return username.count > 0 && username.count < 10 && password.count > 0 && password.count < 8
+        
+    }
+    
+    func navgiateToNextScreen() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let objNextVC = storyboard.instantiateViewController(withIdentifier: "NextVC") as? NextVC
+        
+        if let nextVC = objNextVC {
+            self.navigationController?.pushViewController(nextVC, animated: true)
+        }
+    }
 }
 
